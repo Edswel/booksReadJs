37e33b1940144def8adfdddcb6f5de65abd08280
Edswel <franckini@yahoo.com>
2022-10-23 19:47:27 +0100

    Add book to UI functionality

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ function UI() {
 
 }
 
+// Add new book functionality
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  // Create table row element
+  const row = document.createElement('tr');
+  // Insert columns
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.review}</td>
+    <td><a href='#' class='delete'>X</a></td>
+  `;
+  list.appendChild(row);
+}
+
+// Clear fields functionality
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('review').value = '';
+}
+
 // Events
 document.getElementById('book-form').addEventListener('submit', (e) => {
   // Get input values
@@ -20,6 +42,15 @@ document.getElementById('book-form').addEventListener('submit', (e) => {
   // Instantiate a book
   const book = new Book(title, author, review);
 
-  console.log(book);
+  // Instantiate UI
+  const ui = new UI();
+  console.log(ui);
+
+  // Add new book
+  ui.addBookToList(book);
+
+  // Clear input fields
+  ui.clearFields();
+
   e.preventDefault();
 });
